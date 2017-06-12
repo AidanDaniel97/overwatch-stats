1eccade8b41b7ca7aaf599df4c0ec88e49ad7ca2
Aidan Daniel <aidan.d@wearedrum.com>
2017-06-12 17:37:29 +0100

    Feed now pulls in twitch streams Font awesome icons added

--- src/components/mainFeed.vue
+++ src/components/mainFeed.vue
@@ -2,8 +2,29 @@
 
 	<div @mousedown="dragWindow" class="mainFeed">
 
+
+
+					<div class="container-fluid">
+						<div class="row">
+
+							<div class="col-xs-4 feed-tile-small">
+								<i class="fa fa-pie-chart" aria-hidden="true"></i>
+							</div>
+
+							<div class="col-xs-4 feed-tile-small">
+								<i class="fa fa-cog" aria-hidden="true"></i>
+							</div>
+
+							<div class="col-xs-4 feed-tile-small">
+								<i class="fa fa-info" aria-hidden="true"></i>
+							</div>
+
+					</div>
+				</div>
+
+
 			<div class="container-fluid scroll-tiles">
-				<div class="row" id="scroll-area">
+				<div class="row scroll-area" id="scroll-area" >
 
 					<div class="col-xs-6 feed-tile-medium" style="background-color:purple">
 						<div class="">
@@ -31,6 +52,9 @@
 
 			</div>
 		</div>
+
+
+
 		<div class="container-fluid">
 			<div class="row">
 
@@ -41,28 +65,26 @@
 				</div>
 
 		</div>
-		<div class="row">
+	</div>
 
-			<div class="col-xs-6 feed-tile-medium" style="background-color:blue">
-				<div class="">
-						<p>testing 123</p>
+	<div class="container-fluid scroll-tiles">
+		<div class="row scroll-area" id="streams-scroll-area" >
+				<div v-for="stream in twitchStreams" class="col-xs-6 feed-tile-medium" :style="{ 'background-image': 'url(' + stream.preview.medium + ')' }" style="padding:0px;cursor:pointer;">
+					<a target="_blank" :href="stream.channel.url"><div class="stream-tile-overlay"></div>
+					<div class="col-xs-12">
+						<h2 class="stream-tile-name">{{stream.channel.name}}</h2>
+						<div class="col-xs-12">
+							<p><i class="fa fa-user" aria-hidden="true"></i> {{stream.viewers.toLocaleString()}}</p>
+						</div>
+					</div>
+				</a>
 				</div>
-			</div>
 
-			<div class="col-xs-6 feed-tile-medium" style="background-color:orange">
-				<div class="">
-
-				</div>
-			</div>
-
-			<div class="col-xs-6 feed-tile-medium" style="background-color:grey">
-				<div class="">
-
-				</div>
-			</div>
 
 	</div>
-		</div>
+</div>
+
+
 	</div>
 </template>
 
@@ -71,11 +93,13 @@
 		name: "mainFeed",
 		data(){
 			return{
-				json_response: "No response data"
+				json_response: "No response data",
+				twitchStreams: []
 			}
 		},
-		created() {
-        setTimeout(function() {
+		created: function() {
+
+				setTimeout(function() {
 					$('#scroll-area').on('mousewheel', function(event) {
 							console.log(event.deltaX, event.deltaY, event.deltaFactor, event.currentTarget.scrollLeft, event, event.originalEvent);
 							if (event.originalEvent.deltaY == -100){
@@ -84,7 +108,30 @@
 									this.scrollLeft += 50;
 							}
 					});
+
+					$('#streams-scroll-area').on('mousewheel', function(event) {
+							console.log(event.deltaX, event.deltaY, event.deltaFactor, event.currentTarget.scrollLeft, event, event.originalEvent);
+							if (event.originalEvent.deltaY == -100){
+								this.scrollLeft -= 50;
+							}else{
+									this.scrollLeft += 50;
+							}
+					});
         }, 1);
+
+					console.log("Loading streams");
+					this.$http.get('https://api.twitch.tv/kraken/streams/?game=Overwatch&client_id=f7npttcncz50y2s1scs3ze34lh3wse')
+						.then(response => {
+							console.log(response)
+							console.log(this.twitchStreams);
+							for (this.i = 0; this.i < 5; this.i++) {
+							    this.twitchStreams.push(response.body.streams[this.i]);
+							}
+							console.log(this.twitchStreams);
+				});
+
+
+
     },
 		methods:{
 			dragWindow: function(){
@@ -107,8 +154,9 @@
 			        overwolf.windows.minimize(result.window.id);
 			    }
 				});
-			}
-		}
+			},
+
+	}
 	}
 
 
